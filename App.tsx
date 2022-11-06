9f2d2bb6c51e8e8d58f3fdf20d5b2cb828c270b8
TheAxan <88592270+TheAxan@users.noreply.github.com>
2022-11-06 13:27:40 -0500

    added navigation base

--- App.tsx
+++ App.tsx
@@ -1,23 +1,93 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 
 function App() {
   return (
     <>
       <StatusBar barStyle = "light-content" hidden = {false} backgroundColor = "black" translucent = {true}/>
-      <SafeAreaView style={[styles.container]} >
-        <View style={[styles.box]}></View>
-        <View style={[styles.box]} >
-          <Feather name="users" size={40} color="honeydew" />
-          <Feather name="search" size={40} color="white" />
-          <Feather name="user" size={40} color="floralwhite" />
-        </View>
+      <SafeAreaView style={[styles.container]}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name='HomePage' component={MainScreen}></Stack.Screen>
+            <Stack.Screen name='form' component={FormScreen}></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </>
   );
-}
+};
+
+
+const MainScreen = () => {
+  return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = "users";
+            } else if (route.name === 'Search') {
+              iconName = "search";
+            } else if (route.name === 'Profile') {
+              iconName = "user";
+            }
+
+            return <Feather name={iconName as any} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'white',
+          tabBarActiveBackgroundColor: 'limegreen',
+          tabBarInactiveBackgroundColor: 'limegreen',
+          tabBarShowLabel: false,
+
+          headerStyle: {backgroundColor: 'limegreen',},
+          headerTintColor: 'white'
+        })}
+      >
+        <Tab.Screen name='Home' component={ScrollTab}></Tab.Screen>
+        <Tab.Screen name='Search' component={SearchTab}></Tab.Screen>
+        <Tab.Screen name='Profile' component={ProfileTab}></Tab.Screen>
+      </Tab.Navigator>
+  );
+};
+
+const FormScreen = () => {
+  return (
+    <View style={[styles.form]}>
+      <Text>This is the form screen</Text>
+    </View>
+  );
+};
+
+
+const ScrollTab = () => {
+  return (
+    <Text>This is the ScrollTab</Text>
+  );
+};
+
+const SearchTab = () => {
+  return (
+    <Text>This is the SearchTab</Text>
+  );
+};
+
+const ProfileTab = () => {
+  return (
+    <Text>This is the ProfileTab</Text>
+  );
+};
+
 
 const styles = StyleSheet.create({
   // alt colors: yellowgreen darkgreen green forestgreen
@@ -25,6 +95,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'stretch',
     justifyContent: 'space-between',
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
   },
   box: {
     width: 'auto',
