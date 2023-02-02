591ba20ce59ee25d8bd7e406594efe2a4f3b714b
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-02-01 19:12:31 -0500

    established architecture base

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver',
+        {
+          root: ['./'],
+          "alias": { // this is supposed to mirror tsconfig.json
+            '@lib': './src/lib',
+            '@features': './src/features',
+            '@config': './src/config',
+            '@components': './src/components',
+            '@customTypes': './src/customTypes'
+          },
+        },
+      ],
+    ],
   };
 };
