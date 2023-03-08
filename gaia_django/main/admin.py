d800a3471736f4268361115a49c62f076a19c2e0
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-03-07 21:32:21 -0500

    Implemented base of Question and Answer model

--- gaia_django/main/admin.py
+++ gaia_django/main/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Question, Answer
+from django.db import models
+from django.forms import TextInput, Textarea
 
 admin.site.register(User, UserAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('description', 'id', 'prompt_fr')
+    save_as = True
+    formfield_overrides = { 
+        models.CharField : {'widget': TextInput(attrs={'size':'85'})},
+    }
+admin.site.register(Question, QuestionAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'answer_date', 'latest_answer', 'answer_json')
+admin.site.register(Answer, AnswerAdmin)
--- gaia_django/main/models.py
+++ gaia_django/main/models.py
@@ -4,3 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Question(models.Model):
+    description = models.CharField(max_length=50)
+    prompt_fr = models.CharField(max_length=100, blank=True)
+    prompt_en = models.CharField(max_length=100, blank=True)
+    prompt_hint_fr = models.TextField(blank=True)
+    prompt_hint_en = models.TextField(blank=True)
+    prerequisite = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_date = models.DateTimeField(auto_now_add=True)
+    latest_answer = models.BooleanField()
+    answer_json = models.JSONField()
