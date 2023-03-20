ea55e732513e4d9296c071985a60d2c905d449bf
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-03-20 01:35:23 -0400

    added basic question visualizer

--- src/features/scroll/components/ScrollTab.tsx
+++ src/features/scroll/components/ScrollTab.tsx
@@ -1,7 +1,38 @@
-import { Text } from "react-native";
+import { Text, View, FlatList, TextInput } from "react-native";
+import { useEffect, useState } from "react";
+import { styles } from "@styles/styles";
+import { fetchQuestions } from "@features/scroll/api/questions"
+
+
+type ItemProps = {prompt: string, hint: string};
+
+const Item = ({prompt, hint}: ItemProps) => {
+  const [field, setField] = useState('')
+  return <View style={styles.item}>
+    <Text style={styles.question}>{prompt}</Text>
+    <Text style={styles.hint}>{hint}</Text>
+    <View style={styles.boxInput}>
+      <TextInput
+        placeholder={''}
+        textAlign='center'
+        onChangeText={(field) => setField(field)}
+      />
+    </View>
+  </View>
+};
 
 export const ScrollTab = () => {
-  return (
-    <Text>This is the ScrollTab</Text>
-  );
+  const [questions, setQuestions] = useState([])
+  
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      setQuestions(await fetchQuestions())
+    };
+    bootstrapAsync();    
+  }, []);
+
+  return <FlatList
+    data={questions}
+    renderItem={({item}) => <Item prompt={item['prompt_fr']} hint={item['prompt_hint_fr']} />}
+  />
 };
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,3 +1,4 @@
+
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
@@ -40,5 +41,36 @@ export const styles = StyleSheet.create({
   form: {
     flex: 1,
     justifyContent: 'center',
+  },
+
+  item: {
+    backgroundColor: 'lightgrey',
+    padding: 8,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    flex: 1,
+    alignItems: 'center',
+    
+  },
+  
+  boxInput: {
+    backgroundColor: 'whitesmoke',
+    borderRadius: 10,
+    borderColor: 'grey',
+    borderWidth: 1,
+    margin: 8,
+    padding: 2,
+    width: 50,
+  },
+
+  hint: {
+    color: 'grey',
+    textAlign: "justify",
+    fontSize: 10,
+  },
+
+  question: {
+    textAlign: "justify",
   },
 });
