9964cef34527eb373b057657194c50067cbacbbf
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-03-20 01:32:59 -0400

    added fetchQuestions function

---
+++ src/features/scroll/api/questions.ts
@@ -0,0 +1,12 @@
+import { axios } from "@lib/axios";
+
+export function fetchQuestions () {
+  return axios.get('/questions/')
+  .then((response) => response.data)
+  .catch((e) => {
+    if (e.response && e.response.status != 400) {
+      console.error(e);
+    };
+    throw e;
+  });
+};
