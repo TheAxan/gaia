530c90a94f8d741957a799ebf5465540047acd90
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-03-21 02:04:43 -0400

    changed prerequisite and parent admin display

--- gaia_django/main/models.py
+++ gaia_django/main/models.py
@@ -34,6 +34,9 @@ class Question(models.Model):
         ]
     )
 
+    def __str__(self):
+        return f'({self._id}) {self.description}'
+
 class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
