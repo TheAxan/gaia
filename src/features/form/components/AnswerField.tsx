8eca5129b4035a918c8c9f4ee82f6bf9d440fc36
TheAxan <88592270+TheAxan@users.noreply.github.com>
2023-04-17 14:04:35 -0400

    extracted AnswerField

---
+++ src/features/form/components/AnswerField.tsx
@@ -0,0 +1,21 @@
+import { Text } from "react-native";
+
+import { NumberAnswerField } from "@features/form/components/NumberAnswerField";
+import { BinaryAnswerField } from "@features/form/components/BinaryAnswerField";
+import { MultipleAnswerField } from "@features/form/components/MultipleAnswerField";
+
+
+type props = {questionType: string, children: object, id: number}
+
+export const AnswerField = ({questionType, children, id}: props) => {
+  switch (questionType) {
+    case 'number':
+      return <NumberAnswerField id={id}/>
+    case 'binary':
+      return <BinaryAnswerField id={id}/>
+    case 'multiple':
+      return <MultipleAnswerField  id={id} childrenIds={children}/>
+    default:
+      return <Text>Unhandled question questionType</Text>
+  };
+};
--- src/features/form/components/FormItem.tsx
+++ src/features/form/components/FormItem.tsx
@@ -1,33 +1,17 @@
 import { Text, View } from "react-native";
 import { styles } from '@styles/styles'
 
-import { NumberAnswerField } from "@features/form/components/NumberAnswerField";
-import { BinaryAnswerField } from "@features/form/components/BinaryAnswerField";
-import { MultipleAnswerField } from "@features/form/components/MultipleAnswerField";
+import { AnswerField } from "@features/form/components/AnswerField";
 
 
-type ItemProps = {prompt: string, hint: string, questionType: string, children: any};
-type AnswerFieldProps = {questionType: string, children: any}
+type ItemProps = {prompt: string, hint: string, questionType: string, children: object, id: number};
 
-const AnswerField = ({questionType, children}: AnswerFieldProps) => {
-  switch (questionType) {
-    case 'number':
-      return <NumberAnswerField/>
-    case 'binary':
-      return <BinaryAnswerField/>
-    case 'multiple':
-      return <MultipleAnswerField childrenIds={children}/>
-    default:
-      return <Text>Unhandled question questionType</Text>
-  };
+export const FormItem = ({prompt, hint, questionType, children, id}: ItemProps) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.question}>{prompt}</Text>
+      <Text style={styles.hint}>{hint}</Text>
+      <AnswerField questionType={questionType} children={children} id={id}/>
+    </View>
+  );
 };
-
-export const FormItem = ({prompt, hint, questionType, children}: ItemProps) => {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.question}>{prompt}</Text>
-        <Text style={styles.hint}>{hint}</Text>
-        <AnswerField questionType={questionType} children={children}/>
-      </View>
-    );
-  };
